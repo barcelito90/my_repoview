7c3de79ea4db9a054693a582b00b1425eecbbe74
barcelito90 <barcelay@uci.cu>
2024-03-13 11:06:28 -0400

    Update web loging

---
+++ custom_web/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "My module",
+    'version': '1.0',
+    'depends': ['web'],
+    'author': "ok",
+    'category': 'all',
+    'application': False,
+    'installable': True,
+    'description': 'description',
+    'data': [
+        'views/webclient_templates.xml',
+    ],
+}
